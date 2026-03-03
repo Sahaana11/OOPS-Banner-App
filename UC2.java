79731bcfbce624c05137c65badf9c819ae8b4bea
Sahaana <sahaanacse2029@gmail.com>
2026-03-03 09:06:42 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,22 @@
+/**
+ * OopsBannerApp
+ * Use Case 2 - Banner Print
+ *
+ * @author Kaleeswaran E
+ * @version 2.0
+ */
+
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *****   ***** ");
+        System.out.println("*     *  *     *  *       *   *  ");
+        System.out.println("*     *  *     *  *       *    * ");
+        System.out.println(" *****    *****   *       *     *");
+
+    }
+}
