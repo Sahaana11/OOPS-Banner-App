cde345fd66c61b691a82bc4c82a58e2052194f90
Sahaana <sahaanacse2029@gmail.com>
2026-03-03 09:03:50 +0530

    Add files via upload
    UC1

---
+++ OopsBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OopsBannerApp
+ * Use Case 1 - Print OOPS to Console
+ *
+ * @author Kaleeswaran E
+ * @version 1.0
+ */
+
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
